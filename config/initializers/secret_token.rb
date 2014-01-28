c9c0a935f5c8e7b1a5eea346a725eb0b748e8a36
Alicia French <alienor18@yahoo.com>
2014-01-27 21:46:57 -0800

    week3 railscast assignment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '2c19bb8e5b700627cc1f319330d48e3f52e491e4185b4aa18d55bbd6bfeea18f6ce89cee9d19e8a71f1f4da2f61c85f421ad015e9302a212ac3998465d1852db'
